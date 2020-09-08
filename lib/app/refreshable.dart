4a239a131e8d06c6fa79f2e4ba8fa765fe56e50e
trannamtrung1st <trannamtrung1st@gmail.com>
2020-09-08 10:21:20 +0700

    reset all

--- lib/app/refreshable.dart
+++
@@ -1,7 +0,0 @@
-abstract class Refreshable {
-  bool needRefresh = false;
-
-  void updateKeepAlive();
-
-  void refresh<T>({T refreshParam});
-}
--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-class Constants {
-  static const String API_AUTH = "210.245.20.68:83";
-  static const String TOKEN_DATA_KEY = "token_data";
-  static const String ROLE_MANAGER = "Manager";
-  static const String ROLE_USER = "User";
-  static const String ROLE_ROOM_CHECKER = "RoomChecker";
-}
